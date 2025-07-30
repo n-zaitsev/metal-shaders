c880256ea5cbd7bae3a059a4c8f40d1ebd1cc1c8
Nikita Zaitsev <zajatsnik@gmail.com>
2025-07-30 07:15:07 +0200

    Add sections to content view

--- metal-shaders/Sources/ContentView.swift
+++ metal-shaders/Sources/ContentView.swift
@@ -1,24 +1,53 @@
 import SwiftUI
 
+enum SectionId: Int, Hashable {
+    case `static`
+    case infinite
+    case configurable
+
+    var title: String {
+        switch self {
+        case .static:
+            return "Static"
+        case .infinite:
+            return "Infinite animations"
+        case .configurable:
+            return "Configurable animations"
+        }
+    }
+}
+
 struct ViewModel {
-    let items: [ContentItem]
+    let items: [SectionId: [ContentItem]]
 }
 
 public struct ContentView: View {
-    @State private var viewModel: ViewModel = .init(items: ContentItem.allCases)
-
+    @State private var viewModel: ViewModel = .init(items: [
+        .static: [.heart],
+        .infinite: [.animatedGradient, .blackWhiteCircles, .neonCircles, .archimedeanSpiral],
+        .configurable: [.rhodonea],
+    ])
+    
     public var body: some View {
         NavigationStack {
-            List(viewModel.items) { item in
-                NavigationLink(item.title) {
-                    MetalViewRepresentable(animationSpeed: item.animationSpeed, contentItem: item)
-                        .navigationTitle(item.title)
+            List {
+                ForEach(viewModel.items.keys.sorted(by: { $0.rawValue < $1.rawValue }), id: \.self) { section in
+                    Section(header: Text(section.title)) {
+                        ForEach(viewModel.items[section] ?? [], id: \.self) { item in
+                            NavigationLink(item.title) {
+                                MetalViewRepresentable(
+                                    animationSpeed: item.animationSpeed,
+                                    contentItem: item
+                                )
+                                .navigationTitle(item.title)
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
